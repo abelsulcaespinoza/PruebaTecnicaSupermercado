d6723a2efba76beaeca1a5c7bacbdf02554f1515
Abel Sulca Espinoza <abelsulcaespinoza@gmail.com>
2026-01-29 00:23:52 -0500

    Initial commit

---
+++ src/main/java/org/pruebatecnica/PruebaTecnicaSupermercadoApplication.java
@@ -0,0 +1,13 @@
+package org.pruebatecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaSupermercadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaTecnicaSupermercadoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pruebatecnica/PruebaTecnicaSupermercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pruebatecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaSupermercadoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
